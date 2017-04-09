742e5e52f82ee85c3e9affc4527b2073966bb113
Craig Reynolds <wyldphyre@gmail.com>
2017-04-09 10:29:37 +0800

    Extract AssemblyHelper class

--- Slayer/SlayerConfiguration.cs
+++ Slayer/SlayerConfiguration.cs
@@ -126,20 +126,4 @@ namespace Slayer
       );
     }
   }
-
-  static class AssemblyHelper
-  {
-    public static System.Reflection.AssemblyName ExecutingAssemblyName()
-    {
-      return System.Reflection.Assembly.GetExecutingAssembly().GetName();
-    }
-    public static string ExecutingAssemblyVersion()
-    {
-      return ExecutingAssemblyName().Version.ToString();
-    }
-    public static string CompactExecutingAssemblyVersion()
-    {
-      return ExecutingAssemblyName().Version.ToString().TrimEnd('.', '0');
-    }
-  }
 }
