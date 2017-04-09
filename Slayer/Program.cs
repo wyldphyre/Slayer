7a06aec94856c796dcc245ad6a54b040498d2869
Craig Reynolds <wyldphyre@gmail.com>
2017-04-09 10:28:42 +0800

    Remove unused using references

--- Slayer/Program.cs
+++ Slayer/Program.cs
@@ -1,15 +1,4 @@
-﻿/*! 1 !*/
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Data.SqlClient;
-using System.Windows.Media;
-using System.Diagnostics;
-
-namespace Slayer
+﻿namespace Slayer
 {
   static class Program
   {
